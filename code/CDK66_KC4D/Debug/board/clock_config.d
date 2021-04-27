481af8ffbc03662332c7d0c26a1648467fd3490c
郝常升 <haochangsheng@sjtu.edu.cn>
2021-04-27 22:03:25 +0800

    洋榕gg的代码

--- code/CDK66_KC4D/Debug/board/clock_config.d
+++ code/CDK66_KC4D/Debug/board/clock_config.d
@@ -1,43 +1,43 @@
 board/clock_config.o board/clock_config.d: ../board/clock_config.c \
- E:\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_smc.h \
- E:\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_common.h \
- E:\GitHub\Project2\code\CDK66_KC4D\device/fsl_device_registers.h \
- E:\GitHub\Project2\code\CDK66_KC4D\device/MK66F18.h \
- E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/core_cm4.h \
- E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_version.h \
- E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_compiler.h \
- E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_gcc.h \
- E:\GitHub\Project2\code\CDK66_KC4D\device/system_MK66F18.h \
- E:\GitHub\Project2\code\CDK66_KC4D\device/MK66F18_features.h \
- E:\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_clock.h \
- E:\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_rtc.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_smc.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_common.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/fsl_device_registers.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/MK66F18.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/core_cm4.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_version.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_compiler.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_gcc.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/system_MK66F18.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/MK66F18_features.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_clock.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_rtc.h \
  ../board/clock_config.h \
- E:\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_common.h
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_common.h
 
-E:\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_smc.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_smc.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_common.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_common.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\device/fsl_device_registers.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/fsl_device_registers.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\device/MK66F18.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/MK66F18.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/core_cm4.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/core_cm4.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_version.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_version.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_compiler.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_compiler.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_gcc.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_gcc.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\device/system_MK66F18.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/system_MK66F18.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\device/MK66F18_features.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/MK66F18_features.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_clock.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_clock.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_rtc.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_rtc.h:
 
 ../board/clock_config.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_common.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\drivers/fsl_common.h:
--- code/CDK66_KC4D/Debug/drivers/fsl_llwu.d
+++ code/CDK66_KC4D/Debug/drivers/fsl_llwu.d
@@ -1,33 +1,33 @@
 drivers/fsl_llwu.o drivers/fsl_llwu.d: ../drivers/fsl_llwu.c \
  ../drivers/fsl_llwu.h ../drivers/fsl_common.h \
- E:\GitHub\Project2\code\CDK66_KC4D\device/fsl_device_registers.h \
- E:\GitHub\Project2\code\CDK66_KC4D\device/MK66F18.h \
- E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/core_cm4.h \
- E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_version.h \
- E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_compiler.h \
- E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_gcc.h \
- E:\GitHub\Project2\code\CDK66_KC4D\device/system_MK66F18.h \
- E:\GitHub\Project2\code\CDK66_KC4D\device/MK66F18_features.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/fsl_device_registers.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/MK66F18.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/core_cm4.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_version.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_compiler.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_gcc.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/system_MK66F18.h \
+ C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/MK66F18_features.h \
  ../drivers/fsl_clock.h
 
 ../drivers/fsl_llwu.h:
 
 ../drivers/fsl_common.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\device/fsl_device_registers.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/fsl_device_registers.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\device/MK66F18.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/MK66F18.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/core_cm4.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/core_cm4.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_version.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_version.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_compiler.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_compiler.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_gcc.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\CMSIS/cmsis_gcc.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\device/system_MK66F18.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/system_MK66F18.h:
 
-E:\GitHub\Project2\code\CDK66_KC4D\device/MK66F18_features.h:
+C:\Users\86181\Documents\GitHub\Project2\code\CDK66_KC4D\device/MK66F18_features.h:
 
 ../drivers/fsl_clock.h:
